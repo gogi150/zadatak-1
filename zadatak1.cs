901ee3c768b4261d3131d6ff641b073106dff5b9
gogi150 <153612601+gogi150@users.noreply.github.com>
2023-12-12 12:44:00 +0100

    Add files via upload

---
+++ zadatak1.cs
@@ -0,0 +1,13 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int a = 100;
+		int b = 3;
+		int x = a/b;
+		int y = 100 % 3;
+		Console.WriteLine(a + " dijeljeno s " + b + " je " + x + " i ostatak je " + y);
+	}
+}
